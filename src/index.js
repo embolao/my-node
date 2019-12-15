ca19cd22c19f9efabdf04d2d7a9e5375540ee231
embolao <corneigh@gmail.com>
2019-12-15 09:15:52 +0100

    Conectando database mongodb

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const config = require ('./server/config');
+//database
+require('./database');
 
 const app = config(express());
-
+//Starting the server
 app.listen(app.get('port'), () => {
     console.log('Server on port', app.get('port'));
 });
